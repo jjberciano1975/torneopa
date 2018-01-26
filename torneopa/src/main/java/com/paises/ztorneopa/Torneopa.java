9cefcada1e7dd397fe9dd488b73569480011fd4b
Javier Juan <jjb@wpsnetwork.com>
2018-01-26 09:17:34 +0100

    main

--- torneopa/src/main/java/com/paises/ztorneopa/Torneopa.java
+++ torneopa/src/main/java/com/paises/ztorneopa/Torneopa.java
@@ -1,6 +1,8 @@
 package com.paises.ztorneopa;
 
+import java.io.File;
 import java.io.IOException;
+
 
 import java.io.PrintWriter;
 
@@ -9,14 +11,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import java.io.File;
+
+
 
 
 /**
  * Servlet implementation class Torneopa
  */
 public class Torneopa extends HttpServlet {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ServletException, LifecycleException {
+		  String webappDirLocation = "src/main/webapp/";
+	        Tomcat tomcat = new Tomcat();
+	        
+	        String webPort = System.getenv("PORT");
+	        if (webPort == null || webPort.isEmpty()) {
+	            webPort = "8080";
+	        }
+
+	        tomcat.setPort(Integer.valueOf(webPort));
+
+	        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+	        System.out.println("configuring app with basedir: "
+	                + new File("./" + webappDirLocation).getAbsolutePath());
+
+	        tomcat.start();
+	        tomcat.getServer().await();
 	}
 	
 	private static final long serialVersionUID = 1L;
